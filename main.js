4772278e99ba8033263f4789f2619a783a66db6d
TatShen <shenets-97@mail.ru>
2024-05-29 13:44:42 +0200

    -introduction task

---
+++ main.js
@@ -0,0 +1,7 @@
+
+const mathFunctions = require('./math');
+
+console.log(mathFunctions.add(5,10))
+console.log(mathFunctions.division(10, 2))
+console.log(mathFunctions.sub(10,5))
+console.log(mathFunctions.multiply(5,10))
