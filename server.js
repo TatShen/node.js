97f13b0c63b5f4c484ab6d80eb91bffadffeb8d6
TatShen <shenets-97@mail.ru>
2024-05-29 16:05:49 +0200

    -add routes

--- server.js
+++ server.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Добро пожаловать в Redev!");
+app.use(express.json)
+
+app.get("/api/hello", (req, res) => {
+  res.send("Привет, Redev!");
 });
+
+app.post("/api/echo", (req, res) => {
+    res.send(req.body)
+})
 
 const PORT = 3000;
 app.listen(PORT, () => {
